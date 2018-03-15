099334ad3c5d02efa458d9342e23d758dad4afa6
hans008 <lishihai2015@gmail.com>
2018-03-15 14:35:31 +0800

    reg

--- spider/crawler/main.go
+++ spider/crawler/main.go
@@ -9,6 +9,7 @@ import (
 	"golang.org/x/text/encoding"
 	"bufio"
 	"golang.org/x/net/html/charset"
+	"regexp"
 )
 
 func main() {
@@ -38,7 +39,18 @@ func main() {
 	}
 
 	fmt.Printf("%s\n", all)
+	printCityList(all)
 
+}
+
+func printCityList(contents []byte){
+	re :=regexp.MustCompile(`<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`)
+	//matches := re.FindAll(contents,-1)
+	matches := re.FindAllSubmatch(contents,-1)
+	for _,m := range matches {
+		fmt.Printf("City: %s, URL: %s\n",m[2],m[1])
+	}
+	fmt.Printf("Matches found : %d\n",len(matches))
 }
 
 
